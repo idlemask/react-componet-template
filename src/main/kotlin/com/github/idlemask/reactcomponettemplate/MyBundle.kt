dcad5a3ade38c602f2315a62f11741ac754ed92c
GitHub Action <action@github.com>
2022-05-31 12:51:00 +0000

    Template cleanup

--- src/main/kotlin/com/github/idlemask/reactcomponettemplate/MyBundle.kt
+++ src/main/kotlin/com/github/idlemask/reactcomponettemplate/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.idlemask.reactcomponettemplate
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
---
+++ src/main/kotlin/com/github/idlemask/reactcomponettemplate/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.idlemask.reactcomponettemplate.services
+
+import com.github.idlemask.reactcomponettemplate.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
